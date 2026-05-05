756ef7192bed8964f4f5cd312a6901deac082852
kangmiru <mirufadz25@gmail.com>
2026-05-05 08:28:12 +0700

    merubah title tipe d

--- app/ui/title.tsx
+++ app/ui/title.tsx
@@ -28,8 +28,8 @@ export function TitleTypeC({title}:TitleProps){
 
 export function TitleTypeD({title}:TitleProps){
     return (
-        <h1 className=" capitalize text-3xl pt-5 font-semibold tracking-wide">
+        <h2 className="capitalize text-3xl pt-5 font-semibold tracking-wide">
             {title}
-        </h1>
+        </h2>
     )
 }
